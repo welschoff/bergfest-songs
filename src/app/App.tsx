87da356fe830bc351063323a8e4a593e5b30bf5a
welschoff <h.welschoff@gmx.de>
2021-11-03 12:07:10 +0100

    :sparkles: Add Design for Landing Page
    Add Design for Landing Page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './App.module.css';
 
 function App(): JSX.Element {
-  return <main>Hello 🐟</main>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>Bergfest</h1>
+        <form className={styles.form}>
+          <input type="text" placeholder="First name" />
+          <input type="text" placeholder="Last name" />
+          <input type="submit" className={styles.submit} />
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default App;
