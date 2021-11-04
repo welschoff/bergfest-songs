20c90e6536340796b25f66465a7dc4b634867c7b
welschoff <h.welschoff@gmx.de>
2021-11-04 20:44:34 +0100

    :sparkles: Add a form for Artist and Title

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 import Registration from './components/Title/Registration/Registration';
+import Songform from './components/Title/Songs/Songs';
 import Title from './components/Title/Title';
 
 function App(): JSX.Element {
+  const [selectedUserName, setSelectedUserName] = useState<string | null>(null);
+
+  console.log(selectedUserName);
+
+  let content;
+
+  if (selectedUserName) {
+    content = <Songform />;
+  } else {
+    content = <Registration onSelectUserName={setSelectedUserName} />;
+  }
+
   return (
     <main className={styles.container}>
       <div className={styles.card}>
-        <Title text="Bergfest" />
-        <Registration />
+        <Title
+          text={selectedUserName ? `Hi ${selectedUserName}` : 'Bergfest'}
+        />
+        {content}
       </div>
     </main>
   );
--- src/app/components/Title/Registration/Registration.tsx
+++ src/app/components/Title/Registration/Registration.tsx
@@ -1,25 +1,76 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './Registration.module.css';
 
-function Registration(): JSX.Element {
+type User = {
+  firstName: string;
+  lastName: string;
+};
+
+type RegistrationProps = {
+  onSelectUserName: (userName: string) => void;
+};
+
+function Registration({ onSelectUserName }: RegistrationProps): JSX.Element {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
 
-  console.log(firstName);
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    fetch('https://json-server.machens.dev/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+      }),
+    });
+  }
+
+  function handleFirstNameChange(event: ChangeEvent<HTMLInputElement>) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event: ChangeEvent<HTMLInputElement>) {
+    setLastName(event.target.value);
+  }
+
+  async function handleSelectClick() {
+    const response = await fetch('https://json-server.machens.dev/users');
+    const newUsers = await response.json();
+    setUsers(newUsers);
+  }
+
+  const userOptions = users.map((user) => (
+    <option>
+      {user.firstName} {user.lastName}
+    </option>
+  ));
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <select
+        onClick={handleSelectClick}
+        onChange={(event) => onSelectUserName(event.target.value)}
+      >
+        <option>Select user</option>
+        {userOptions}
+      </select>
+      or
       <input
         type="text"
         placeholder="First name"
         value={firstName}
-        onChange={(event) => setFirstName(event.target.value)}
+        onChange={handleFirstNameChange}
       />
       <input
         type="text"
         placeholder="Last name"
         value={lastName}
-        onChange={(event) => setLastName(event.target.value)}
+        onChange={handleLastNameChange}
       />
       <input type="submit" className={styles.submit} />
     </form>
